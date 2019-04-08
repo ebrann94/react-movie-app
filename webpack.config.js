736adf9084eed78ac3e99ae145a1df7dac391ea2
Ethan Brann <ethan.brann94@gmail.com>
2019-04-08 11:53:16 +0100

    added style files

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.join(__dirname, 'public'),
+        path: path.join(__dirname, 'dist'),
         filename: 'bundle.js'
     },
     module: {
