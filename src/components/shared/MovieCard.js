5bc523a7395d16f25783716d670de981227d4bdf
Ethan Brann <ethan.brann94@gmail.com>
2019-05-24 23:40:05 +0100

    added vote average to MovieCard

--- src/components/shared/MovieCard.js
+++ src/components/shared/MovieCard.js
@@ -4,7 +4,18 @@ import { Link } from 'react-router-dom';
 const MovieCard = (props) => {
     return (
         <div className="movie">
-            <h3 className="movie__title">{props.title} <span className="movie__year">({props.releaseYear})</span></h3>
+            <div className="movie__header">
+                <h3 className="movie__title">{props.title} <span className="movie__year">({props.releaseYear})</span></h3>
+                <div className="movie__vote-container">
+                    <p className="movie__vote">{props.voteAverage}</p>
+                    <img 
+                        src="/images/gold_star.png" 
+                        alt="Gold Star" 
+                        width="32px"
+                        className="movie__gold-star"
+                    />
+                </div>
+            </div>
             <div className="movie__info-wrapper">
                 {props.posterPath && <img src={`https://image.tmdb.org/t/p/original${props.posterPath}`} className="movie__poster"/>}
                 <div className="extra-info">
